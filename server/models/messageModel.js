204c2b400aae4e223ce98badc23a328246262286
Nicholas Kneafsey <nicholaskneafsey@gmail.com>
2016-01-25 12:07:52 -0800

    Improved schema for messages and controller

--- server/models/messageModel.js
+++ server/models/messageModel.js
@@ -2,14 +2,14 @@ var db = require('../db');
 
 module.exports = {
   get: function (params, callback) {
-    var queryStr = "SELECT * FROM Messages WHERE houseId=? LIMIT 50";
+    var queryStr = "SELECT users.name, messages.text, messages.time FROM messages LEFT OUTER JOIN users ON (messages.userid=users.id) WHERE messages.houseId=? LIMIT 50";
     db.query(queryStr, params, function (err, results) {
       callback(err, results);
     });
   },
 
   post: function (params, callback) {
-    var queryStr = "INSERT INTO Messages (userId, text, houseId) VALUES (?,?,?)";
+    var queryStr = "INSERT INTO Messages (userId, text, houseId, time) VALUES (?,?,?, NOW())";
     db.query(queryStr, params, function(err, results) {
       callback(err, results);
     })
