0387b6b6364164ab0a977f8a9a621b5e3e7f5d51
Nicholas Kneafsey <nicholaskneafsey@gmail.com>
2016-02-01 20:51:34 -0800

    Changed order

--- server/db/index.js
+++ server/db/index.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 
 var connection = mysql.createConnection({
+  host: process.env.DBHOST || null,
   user: process.env.DBUSER || "root",
   password: process.env.DBPASS || "",
-  database: process.env.DATABASE || "bananas",
-  host: process.env.DBHOST || null
+  database: process.env.DATABASE || "bananas"
 });
 
 connection.connect();
