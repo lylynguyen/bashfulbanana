fea02b3b2e4cffbeb13371d7ccc36a1fd85c1819
Joey Holland <joeyholland15@Joeys-MacBook-Pro.local>
2016-02-10 16:33:11 -0800

    gets mobile front end hooked up to the database

--- server/controllers/mobileMessageController.js
+++ server/controllers/mobileMessageController.js
@@ -2,23 +2,12 @@ var mobileMessageModel = require('../models/mobileMessageModel.js');
 
 module.exports = {
   get: function(req, res){
-  var params = [houseId];
-    mobileMessageModel.get(params, function(err, results) {
-      if (err) {
-        res.sendStatus(500);
-      }
-      res.json(results);
-    });
-  },
-
-  post: function(req, res){
-    var params = [houseId];
-    mobileMessageModel.post(params, function(err, results){
-      if(err) {
-        res.sendStatus(500);
-      }
-      res.json(results);
-    })
-  }
-
+    var params = [req.params.houseId];
+      mobileMessageModel.get(params, function(err, results) {
+        if (err) {
+          res.sendStatus(500);
+        }
+        res.json(results);
+      });
+    }
 }
--- server/models/mobileMessageModel.js
+++ server/models/mobileMessageModel.js
@@ -1,17 +1,13 @@
-// var db = require('../db');
+var db = require('../db');
 
-// module.exports = {
-//   get: function(params, callback){
-//     var queryStr = "";
-//     db.queryStr(queryStr, params, function(){
-//       callback(err, results);
-//     })
-//   },
+module.exports = {
+  get: function (params, callback) {
+    var queryStr = "SELECT Users.name, Users.userImageUrl, Messages.text, DATE_FORMAT(Messages.time,'%W, %M %e, %Y %h:%i %p') AS time FROM Messages LEFT OUTER JOIN Users ON (Messages.userid=Users.id) WHERE Messages.houseId=? AND landlordChat=0 ORDER BY Messages.time DESC LIMIT 50";
+    //var queryStr = 'SELECT * from Users'
+    db.query(queryStr, params, function (err, results) {
+      callback(err, results);
+    });
+  }
+}
 
-//   post: function(params, callback) {
-//     var queryStr = "";
-//     db.query(queryStr, params, function() {
-//       callback(err, results);
-//     });
-//   },
-// }
+
